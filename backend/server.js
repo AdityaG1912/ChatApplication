798758645121843070bef6cb77815d33f568d0e2
AdityaG1912 <aditya.gaikwad2013@gmail.com>
2024-08-30 12:31:12 +0530

    register user and login user authentication handle and generating jwt token for authentication and authorization

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { chats } = require("./data/data");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 
@@ -9,8 +10,11 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Express api is running");
 });
+
+app.use("/api/user", userRoutes);
 
 app.listen(port, console.log(`Server started on port ${port}`));
